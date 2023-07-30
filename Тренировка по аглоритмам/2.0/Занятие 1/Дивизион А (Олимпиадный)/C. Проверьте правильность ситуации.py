23fff24f92764cf4074df7b3cc163b223ed3b3bb
Aleksey Evdakov <eae_rus@mail.ru>
2023-07-30 15:58:53 +0300

    Изменена "Занятие 1 (A), C. Проверьте правильность ситуации
    Добавил проверку правильного колличества точек при условии победы.
    Задача всё равно не принята, надо ещё что-то искать.

--- Тренировка по аглоритмам/2.0/Занятие 1/Дивизион А (Олимпиадный)/C. Проверьте правильность ситуации.py
+++ Тренировка по аглоритмам/2.0/Занятие 1/Дивизион А (Олимпиадный)/C. Проверьте правильность ситуации.py
@@ -2,54 +2,60 @@ def main():
     '''
     '''
     def is_right_situation(list_points):
-        def is_count_points_correct(list_points):
-            '''
-            проверка на количество точек
-            '''
-            count_1 = 0
-            count_2 = 0
-            for points in list_points:
-                for point in points:
-                    if point == 1:
-                        count_1 += 1
-                    elif point == 2:
-                        count_2 += 1
-            if abs(count_1 - count_2) > 1:
-                return False
-            return True
-        
-        def is_correct_lines_count(list_points):
-            '''
-            проверка на ситуацию по количеству линий одинаковых цифр
-            '''
-            count_equal_lines = 0
-            for i in range (3):
-                # проверка по x
-                equal_lines_x = True
-                for j in range(2):
-                    if (list_points[i][j] != list_points[i][j+1]) or list_points[i][j] == 0:
-                        equal_lines_x = False
-                        break
-                if equal_lines_x:
-                    count_equal_lines += 1
-                    
-                # проверка по y
-                equal_lines_y = True
-                for j in range (2):
-                    if (list_points[j][i] != list_points[j+1][i]) or list_points[j][i] == 0:
-                        equal_lines_y = False
-                        break
-                if equal_lines_y:
-                    count_equal_lines += 1
-                    
-            if count_equal_lines > 1:
-                return False
-            return True
-
-        if is_count_points_correct(list_points) and is_correct_lines_count(list_points):
-            return True
-        else:
+        # проверка на количество точек
+        count_cross = 0
+        count_zero = 0
+        for points in list_points:
+            for point in points:
+                if point == 1:
+                    count_cross += 1
+                elif point == 2:
+                    count_zero += 1
+        diff = count_cross - count_zero
+        if diff > 1 or diff < 0:
             return False
+        
+        # проверка на ситуацию по количеству линий одинаковых цифр
+        count_equal_lines = 0
+        is_equal_lines_cross = False
+        is_equal_lines_zero = False
+        for i in range (3):
+            # проверка по x
+            equal_lines_x = True
+            for j in range(2):
+                if (list_points[i][j] != list_points[i][j+1]) or list_points[i][j] == 0:
+                    equal_lines_x = False
+                    break
+            if equal_lines_x:
+                count_equal_lines += 1
+                if list_points[i][0] == 1: # крестик
+                    is_equal_lines_cross = True
+                else: # нолик
+                    is_equal_lines_zero = True
+                
+            # проверка по y
+            equal_lines_y = True
+            for j in range (2):
+                if (list_points[j][i] != list_points[j+1][i]) or list_points[j][i] == 0:
+                    equal_lines_y = False
+                    break
+            if equal_lines_y:
+                count_equal_lines += 1
+                if list_points[0][i] == 1: # крестик
+                    is_equal_lines_cross = True
+                else: # нолик
+                    is_equal_lines_zero = True
+                
+        if count_equal_lines > 1:
+            return False
+        
+        # проверка обобщения
+        if is_equal_lines_cross and (diff != 1):
+            return False
+        elif is_equal_lines_zero and (diff != 0):
+            return False
+                
+        return True
     
     
     list_points = []

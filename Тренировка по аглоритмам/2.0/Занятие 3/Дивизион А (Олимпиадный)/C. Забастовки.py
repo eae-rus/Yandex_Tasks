c216e13170079c07818c333cd2be178217e518cc
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-05 23:11:51 +0300

    Изменена "Занятие 2 (A), C. Забастовки"
    Переписал условие. Хмм... Даже немного стыдно, глупая ошибка с днями недели. Да, теперь всё ок.

--- Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/C. Забастовки.py
+++ Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/C. Забастовки.py
@@ -3,12 +3,18 @@ def main():
     '''
     N, K = list(map(int, input().split()))
 
+    days_off = set()
+    for _ in range(6, N+1, 7):
+        days_off.add(_)
+    for _ in range(7, N+1, 7):
+        days_off.add(_)
+        
     all_strikes = set()
     for _ in range(K):
         a, b = list(map(int, input().split()))
 
         for i in range(a, N+1, b):
-            if i % 6 == 0 or i % 7 == 0 or i in all_strikes:
+            if i in days_off or i in all_strikes:
                 continue
             
             all_strikes.add(i)

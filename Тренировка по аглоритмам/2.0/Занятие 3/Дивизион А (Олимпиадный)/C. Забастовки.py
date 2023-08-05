993a5fd46d96dce232c4904faa8fd315353e1867
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-05 23:04:07 +0300

    Изменена "Занятие 2 (A), C. Забастовки"
    Переписал условие. Почему задача не заходит - большая загадка...

--- Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/C. Забастовки.py
+++ Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/C. Забастовки.py
@@ -6,15 +6,12 @@ def main():
     all_strikes = set()
     for _ in range(K):
         a, b = list(map(int, input().split()))
-        
-        # Закоментил. Но в условии не сказано, что нужно выводить только те дни
-        # когда наносится финансовый ущерб.
-        #for i in range(a, N+1, b):
-        #    all_strikes.add(i)
 
         for i in range(a, N+1, b):
-            if i % 6 != 0 and i % 7 != 0:
-                all_strikes.add(i)
+            if i % 6 == 0 or i % 7 == 0 or i in all_strikes:
+                continue
+            
+            all_strikes.add(i)
     
     print(len(all_strikes))  
     

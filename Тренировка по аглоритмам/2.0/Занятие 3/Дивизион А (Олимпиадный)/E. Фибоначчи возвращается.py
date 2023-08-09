da6c8e0eed115c22f2e4e10b2aa8a40aafe1a44f
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-09 21:35:12 +0300

    Изменена "Занятие 2 (A), E.Фибоначчи возвращается"
    Первая попытка, но она не зашла

--- Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/E. Фибоначчи возвращается.py
+++ Тренировка по аглоритмам/2.0/Занятие 3/Дивизион А (Олимпиадный)/E. Фибоначчи возвращается.py
@@ -1,28 +1,26 @@
 def main():
     '''
     '''
-    def add_Fibonacci(list_Fibonacci, set_Fibonacci, max_number):
-        while True:
-            list_Fibonacci.append(list_Fibonacci[-1] + list_Fibonacci[-2])
-            set_Fibonacci.add(list_Fibonacci[-1])
-            if list_Fibonacci[-1] > max_number:
-                break
-        return (list_Fibonacci, set_Fibonacci)
-        
     N = int(input())
 
+    A = [0] * N
+    for i in range(N):
+        A[i] = int(input())
+        
+    max_number = max(A)
+    
     list_Fibonacci = []
     list_Fibonacci.append(1)
     list_Fibonacci.append(1)
+    while True:
+        list_Fibonacci.append(list_Fibonacci[-1] + list_Fibonacci[-2])
+        if list_Fibonacci[-1] > max_number:
+            break
     
     set_Fibonacci = set(list_Fibonacci)
-
-    for _ in range(N):
-        number = int(input())
-        if number > list_Fibonacci[-1]:
-            list_Fibonacci, set_Fibonacci = add_Fibonacci(list_Fibonacci, set_Fibonacci, number)
-
-        if number in set_Fibonacci:
+    
+    for i in range(len(A)):
+        if A[i] in set_Fibonacci:
             print('YES')
         else:
             print('NO')

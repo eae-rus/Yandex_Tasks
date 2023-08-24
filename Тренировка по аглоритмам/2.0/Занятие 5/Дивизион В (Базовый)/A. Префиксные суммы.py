9ca410bf36a0848a14d87d61a040f321ada7b492
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-24 22:20:10 +0300

    Изменена "Занятие 5 (B), A. Префиксные суммы"
    Удалил вызов функции. Хотя с чего вдруг это должно задержки вносить?
    
    Не меняет, сейчас попробую язык поменять, но это ересь будет.

--- Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
+++ Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
@@ -1,20 +1,11 @@
-def main():
-    '''
-    '''
-    n,q = map(int, input().split())
-    a_prefix_sum = list(map(int, input().split()))
-    
-    for i in range(1, n):
-        a_prefix_sum[i] = a_prefix_sum[i] + a_prefix_sum[i-1]
+n,q = map(int, input().split())
+a_prefix_sum = list(map(int, input().split()))
 
-    for _ in range(q):
-        l,r = map(int, input().split())
-        if l == 1:
-            print(a_prefix_sum[r-1])
-        else:
-            print(a_prefix_sum[r-1] - a_prefix_sum[l-2])
-    
-
-    
-if __name__ == '__main__':
-	main()
+for i in range(1, n):
+    a_prefix_sum[i] = a_prefix_sum[i] + a_prefix_sum[i-1]
+for _ in range(q):
+    l,r = map(int, input().split())
+    if l == 1:
+        print(a_prefix_sum[r-1])
+    else:
+        print(a_prefix_sum[r-1] - a_prefix_sum[l-2])

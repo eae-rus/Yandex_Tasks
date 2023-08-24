0f84f9894fc51675998ea990f6134fb7ef4b733b
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-24 22:04:26 +0300

    Изменена "Занятие 5 (B), A. Префиксные суммы"
    Попробовал иначе задавать массивы - не помогло.

--- Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
+++ Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
@@ -4,9 +4,9 @@ def main():
     n,q = map(int, input().split())
     a = list(map(int, input().split()))
     
-    a_prefix_sum = [0] * (n+1)
+    a_prefix_sum = [0]
     for i in range(n):
-        a_prefix_sum[i+1] = a_prefix_sum[i] + a[i]
+        a_prefix_sum.append(a_prefix_sum[i] + a[i])
 
     for _ in range(q):
         l,r = map(int, input().split())

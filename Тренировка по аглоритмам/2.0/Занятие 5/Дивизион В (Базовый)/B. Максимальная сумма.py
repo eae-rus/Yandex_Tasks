56d54aea7fc188448e5888bbf09bc108efd24622
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-24 23:20:53 +0300

    Изменена "Занятие 5 (B), B. Максимальная сумма"
    Теперь я накапливаю префексную сумму до тех пор, пока она не занулилась. Ведь может быть маленький минус, и большие плюсы, а значит, минус нам не страшен.
    
    Да, задачка решилась.

--- Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/B. Максимальная сумма.py
+++ Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/B. Максимальная сумма.py
@@ -7,15 +7,14 @@ def main():
     a_prefix_sum = [0] * (n+1)
     max_sum = a[0]
     for i in range(n):
-        if a[i] >= 0:
+        if a_prefix_sum[i] + a[i] >= 0:
             a_prefix_sum[i+1] = a_prefix_sum[i] + a[i]
             if a_prefix_sum[i+1] > max_sum:
                 max_sum = a_prefix_sum[i+1]
         else:
-            a_prefix_sum[i+1] = 0
             if a[i] > max_sum:
                max_sum = a[i]
-      
+     
     print(max_sum)
 
 if __name__ == '__main__':

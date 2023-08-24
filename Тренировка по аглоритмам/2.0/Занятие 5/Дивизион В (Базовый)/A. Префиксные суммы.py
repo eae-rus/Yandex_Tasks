14955a3fb654738878f83ae257e6470248f4d583
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-24 22:24:26 +0300

    Изменена "Занятие 5 (B), A. Префиксные суммы"
    Да, дело в скорости обработки, может какой-то из вариантов был и чуть лучше этого, но сиё не важно. Для случаев, когда очень большие числа, лучше использовать Python 3.9 (PyPy 7.3.11)

--- Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
+++ Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/A. Префиксные суммы.py
@@ -1,11 +1,18 @@
-n,q = map(int, input().split())
-a_prefix_sum = list(map(int, input().split()))
+def main():
+    '''
+    '''
+    n,q = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    a_prefix_sum = [0] * (n+1)
+    for i in range(n):
+        a_prefix_sum[i+1] = a_prefix_sum[i] + a[i]
 
-for i in range(1, n):
-    a_prefix_sum[i] = a_prefix_sum[i] + a_prefix_sum[i-1]
-for _ in range(q):
-    l,r = map(int, input().split())
-    if l == 1:
-        print(a_prefix_sum[r-1])
-    else:
-        print(a_prefix_sum[r-1] - a_prefix_sum[l-2])
+    for _ in range(q):
+        l,r = map(int, input().split())
+        print(a_prefix_sum[r] - a_prefix_sum[l-1])
+    
+
+    
+if __name__ == '__main__':
+	main()

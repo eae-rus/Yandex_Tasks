3c685a82e8b4844b217867213d68518a1556f07a
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-24 22:44:10 +0300

    Изменена "Занятие 5 (B), B. Максимальная сумма"
    Скачал входные данные, да, я не достаточно хорошо проработал вопрос отрицательных значений.
    
    Вот этот тест:
    5
    450402558 -840167367 -231820501 586187125 -627664644

--- Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/B. Максимальная сумма.py
+++ Тренировка по аглоритмам/2.0/Занятие 5/Дивизион В (Базовый)/B. Максимальная сумма.py
@@ -2,13 +2,21 @@ def main():
     '''
     '''
     n = int(input())
-    a_prefix_sum = list(map(int, input().split()))
+    a = list(map(int, input().split()))
     
-    max_sum = a_prefix_sum[0]
-    for i in range(1, n):
-        a_prefix_sum[i] = a_prefix_sum[i-1] + a_prefix_sum[i]
-        if a_prefix_sum[i] > max_sum:
-            max_sum = a_prefix_sum[i]
+    a_prefix_sum = [0] * (n+1)
+    max_sum = a[0]
+    for i in range(n):
+        if a[i] >= 0:
+            a_prefix_sum[i+1] = a_prefix_sum[i] + a[i]
+        else:
+            a_prefix_sum[i+1] = 0
+        
+        if a_prefix_sum[i+1] > max_sum:
+            max_sum = a_prefix_sum[i+1]
+        
+        if a[i] > max_sum:
+            max_sum = a[i]
 
     print(max_sum)
 

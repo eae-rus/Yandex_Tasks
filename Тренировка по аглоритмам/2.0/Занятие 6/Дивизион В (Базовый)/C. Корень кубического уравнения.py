1af68abf9386ae09c179567dfc03bd5cb447a4c0
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-26 10:08:52 +0300

    Изменена "Занятие 5 (B), C. Корень кубического уравнения"
    Большие претензии к их алгоритму, там не реальный корень, а первое при поиске справа... АРрр...

--- Тренировка по аглоритмам/2.0/Занятие 6/Дивизион В (Базовый)/C. Корень кубического уравнения.py
+++ Тренировка по аглоритмам/2.0/Занятие 6/Дивизион В (Базовый)/C. Корень кубического уравнения.py
@@ -4,21 +4,21 @@ def main():
     a,b,c,d = list(map(int, input().split()))
     
     left, right = -1000, 1000
-    while right - left > 1e-6:
+    while right - left > 1e-5:
         mid = (left + right) / 2
         y = a * mid**3 + b * mid**2 + c * mid + d
-        if y < 0:
+        if y > 0:
             if a > 0:
-                left = mid
-            else:
                 right = mid
+            else:
+                left = mid
         else:
             if a > 0:
-                right = mid
-            else:
                 left = mid
+            else:
+                right = mid
     
-    print((left + right)/2)
+    print((left + right) / 2)
     
 if __name__ == '__main__':
 	main()

ed6285fe5b16008ea3f16e29c09627e85e1114fd
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-26 20:20:20 +0300

    Изменена "Занятие 7 (B), C. Минимальное покрытие"
    Посмотрел объяснение разбора решения, вроде бы всё нормально понял, будем смотреть по тестам.
    
    Ага, на первом тесте ошибка теперь...

--- Тренировка по аглоритмам/2.0/Занятие 7/Дивизион В (Базовый)/C. Минимальное покрытие.py
+++ Тренировка по аглоритмам/2.0/Занятие 7/Дивизион В (Базовый)/C. Минимальное покрытие.py
@@ -13,39 +13,28 @@ def main():
         if r <= 0 or l >= M:
             continue
         
-        events.append((l,-1,(l,r)))
-        events.append((r,1,(l,r)))
+        events.append((l,r))
         
     events = sorted(events)
     
     new_array = []
-    line_dict = {}
-    count_line = 0
     is_no_solution = False
+    max_right = 0
+    max_right_line = events[0]
     for event in events:
-        if event[1] == -1:
-            if event[2] not in line_dict:
-                line_dict[event[2]] = 0
-            line_dict[event[2]] += 1
-            count_line += 1
+        if event[0] <= max_right:
+            if event[1] > max_right_line[1]:
+                max_right_line = event
         else:
-            line_dict[event[2]] -= 1
-            if line_dict[event[2]] == 0:
-                del line_dict[event[2]]
-            count_line -= 1
-            if count_line == 0 and event[0] < M:
+            if event[0] > max_right_line[1]:
                 is_no_solution = True
                 break
-            
-            unique = True 
-            for key in line_dict:
-                l,r = key
-                if event[2][0] > l and event[2][1] < r:
-                    unique = False
-                    break
-            
-            if unique:
-                new_array.append(event[2])
+            else:
+                max_right = max_right_line[1]
+                new_array.append(max_right_line)
+                max_right_line = event
+
+    new_array.append(max_right_line)
 
     if is_no_solution or len(new_array) == 0:
         print('No solution')

df6b264e1af6cc94a114f343fc05bb18df31e3e7
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-26 17:48:36 +0300

    + Добавлено "Занятие 6 (В), D. Вырубка леса"
    Попытка 1
    Посмотрел размышления из решений, пришла умная идея, и в целом, можно и без двоичного поиска решать в общем-то.
    
    Да, TL, ну значит сейчас поправим левую границу.

---
+++ Тренировка по аглоритмам/2.0/Занятие 6/Дивизион В (Базовый)/D. Вырубка леса.py
@@ -0,0 +1,22 @@
+def main():
+    '''
+    '''
+    cutting_down_trees_1, rest_1, cutting_down_trees_2, rest_2, sum_trees = map(int, input().split())
+    
+    left = 1
+    right = 3 + sum_trees // (cutting_down_trees_1 * (1-1/rest_1) + cutting_down_trees_2 * (1-1/rest_2))
+    
+    while left < right:
+        mid = (left + right) // 2
+        rest_1_days = mid // rest_1
+        rest_2_days = mid // rest_2
+        cutting_down_trees =  (mid - rest_1_days) * cutting_down_trees_1 + (mid - rest_2_days) * cutting_down_trees_2
+        if cutting_down_trees >= sum_trees:
+            right = mid
+        else:
+            left = mid + 1
+    
+    print(int(right))
+    
+if __name__ == '__main__':
+	main()

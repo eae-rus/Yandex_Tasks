a3dd3db666b4c8ef5233806ac9e5e54c7d9b9dd2
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-26 10:02:47 +0300

    + Добавлено "Занятие 6 (В), C. Корень кубического уравнения"
    Попытка 1
    Наверное, можно как-то сузить рамки поиска.
    
    неправильный ответ - да ересь всё это.

---
+++ Тренировка по аглоритмам/2.0/Занятие 6/Дивизион В (Базовый)/C. Корень кубического уравнения.py
@@ -0,0 +1,24 @@
+def main():
+    '''
+    '''
+    a,b,c,d = list(map(int, input().split()))
+    
+    left, right = -1000, 1000
+    while right - left > 1e-6:
+        mid = (left + right) / 2
+        y = a * mid**3 + b * mid**2 + c * mid + d
+        if y < 0:
+            if a > 0:
+                left = mid
+            else:
+                right = mid
+        else:
+            if a > 0:
+                right = mid
+            else:
+                left = mid
+    
+    print((left + right)/2)
+    
+if __name__ == '__main__':
+	main()

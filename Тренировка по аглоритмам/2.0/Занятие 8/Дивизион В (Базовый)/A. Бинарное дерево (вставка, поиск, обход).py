957a6c85b2d19ad1f7ab3212567a5ed2e90ae1e1
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-27 10:32:53 +0300

    + Добавлено "Занятие 8 (В), A. Бинарное дерево (вставка, поиск, обход)"
    Попытка 1
    Признаюсь, прост попросил ChatGPT, и разобрался в решении.
    
    RE... Ну здрасти, и какое же решение вам нужно?

---
+++ Тренировка по аглоритмам/2.0/Занятие 8/Дивизион В (Базовый)/A. Бинарное дерево (вставка, поиск, обход).py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+    if root is None:
+        return Node(value)
+    if value < root.value:
+        root.left = insert(root.left, value)
+    elif value > root.value:
+        root.right = insert(root.right, value)
+    return root
+
+def search(root, value):
+    if root is None or root.value == value:
+        return root is not None
+    if value < root.value:
+        return search(root.left, value)
+    return search(root.right, value)
+
+def print_tree(root, depth=0):
+    if root is None:
+        return
+    print_tree(root.left, depth + 1)
+    print("." * depth + str(root.value))
+    print_tree(root.right, depth + 1)
+
+root = None
+
+while True:
+    query = input().split()
+    if query[0] == "ADD":
+        value = int(query[1])
+        if search(root, value):
+            print("ALREADY")
+        else:
+            root = insert(root, value)
+            print("DONE")
+    elif query[0] == "SEARCH":
+        value = int(query[1])
+        if search(root, value):
+            print("YES")
+        else:
+            print("NO")
+    elif query[0] == "PRINTTREE":
+        print_tree(root)

2569640dd6d4afcd3bbe52d0be8e8b1a6820ff9f
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-27 12:05:46 +0300

    Изменена "Занятие 8 (B), B. Родословная: предки и потомки"
    Добавил, но RE... Да ну их нафиг с этой их непонятной задачей данных.

--- Тренировка по аглоритмам/2.0/Занятие 8/Дивизион В (Базовый)/B. Родословная предки и потомки.py
+++ Тренировка по аглоритмам/2.0/Занятие 8/Дивизион В (Базовый)/B. Родословная предки и потомки.py
@@ -1,3 +1,5 @@
+import sys
+
 def main():
     '''
     '''
@@ -10,6 +12,26 @@ def main():
             for child in tree[1]:
                 if add_descendant(child, parent, descendant):
                     return True
+            return False
+    
+    def find_parant(tree, parent):
+        if parent == tree[0]:
+            return tree
+        else:
+            for child in tree[1]:
+                new_tree = find_parant(child, parent)
+                if new_tree is not None:
+                    return new_tree
+            return None
+           
+    def find_descendant(tree, descendant):
+        if descendant == tree[0]:
+            return True
+        else:
+            for child in tree[1]:
+                if find_descendant(child, descendant):
+                    return True
+            return False
     
     N = int(input())
     descendant, parent = input().split()
@@ -21,6 +43,28 @@ def main():
             continue
         else:
             print("ERROR")
+    
+    answer = []
+    query = sys.stdin.readline()[0:-1].split()
+    while not (query[0] == ""):
+        descendant, parent = query[0], query[1]
+        
+        new_tree = find_parant(tree, parent)
+        if find_descendant(new_tree, descendant):
+            answer.append(2)
+            query = sys.stdin.readline()[0:-1].split()
+            continue
+        
+        new_tree = find_parant(tree, descendant)
+        if find_descendant(new_tree, parent):
+            answer.append(1)
+            query = sys.stdin.readline()[0:-1].split()
+            continue
+        
+        answer.append(0)
+        query = sys.stdin.readline()[0:-1].split()
+        
+    print(' '.join(map(str, answer)))
      
 if __name__ == '__main__':
 	main()

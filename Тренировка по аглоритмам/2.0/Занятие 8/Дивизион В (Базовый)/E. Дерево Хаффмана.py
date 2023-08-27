7506da711674efe98cdd3654ebc91f71c6ce8640
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-27 21:54:25 +0300

    + Добавлено "Занятие 8 (В), E. Дерево Хаффмана"
    Попытка 1.
    Хотя бы на тестовых данных заработало! Это уже прорыв!
    
    На втором тесте валится, будем разбираться))

---
+++ Тренировка по аглоритмам/2.0/Занятие 8/Дивизион В (Базовый)/E. Дерево Хаффмана.py
@@ -0,0 +1,38 @@
+import sys
+
+def main():
+    '''
+    '''
+    def create_tree(huffman_codes, code, h_s_s):
+        if len(h_s_s) == 0:
+            huffman_codes.append(code)
+            return
+        
+        symbol = h_s_s.pop()
+        if symbol == "U":
+            huffman_codes.append(code)
+            return
+        else: # D
+            if code == "0": # наверное и не нужно (надо уточнить логику кодировки)
+                create_tree(huffman_codes, "0", h_s_s)
+            else:
+                create_tree(huffman_codes, code + "0", h_s_s)
+                
+            create_tree(huffman_codes, code + "1", h_s_s)
+            
+    
+    N = int(input())
+    for _ in range(N):
+        h_s_s = input() # huffman_short_symbol
+        huffman_codes = []
+        # преобразуем в обратный список, чтобы было удобно удалять и обрабатывать.
+        h_s_s = [h_s_s[-i-1] for i in range(len(h_s_s))]
+        
+        create_tree(huffman_codes, "", h_s_s)
+        
+        print(len(huffman_codes))
+        for code in huffman_codes:
+            print(code)
+     
+if __name__ == '__main__':
+	main()

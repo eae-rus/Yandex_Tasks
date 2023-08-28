8bb464bf352a9f37702ef00ab19de482e8c471ac
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-28 22:47:36 +0300

    + Добавлено "3.0 №2. Красивая строка"
    Попытка 1.
    Сперва посмотрел объяснение если честно, не до конца понимал идею, чёт не пришла мысль про 2 указателя, если честно. Решение написал уже сам.
    Странно, TL, хоть должно быть нормально же

---
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/1. Гистограмма/2. Красивая строка.py
@@ -0,0 +1,30 @@
+def main():
+    '''
+    '''
+    k = int(input())
+    stroke = input()
+    characters = set(stroke)
+    
+    max_beauty = 0
+    for char in characters:
+        left, right = 0, 0
+        use_k = 0
+        while left < len(stroke) and right < len(stroke):
+            if left > right:
+                right = left
+                
+            while right < len(stroke):
+                if stroke[right] != char:
+                    if use_k >= k:
+                        break
+                    use_k += 1
+                right += 1
+            max_beauty = max(max_beauty, right - left)
+            if stroke[left] != char:
+                use_k = max(use_k - 1, 0)
+            left += 1
+    
+    print(max_beauty)
+     
+if __name__ == '__main__':
+	main()

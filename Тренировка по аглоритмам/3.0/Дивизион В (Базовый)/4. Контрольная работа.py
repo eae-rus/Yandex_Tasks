22b55089132236c23dc4f7c859eb6bdbb007bad4
Aleksey Evdakov <eae_rus@mail.ru>
2023-08-30 22:22:45 +0300

    Изменена "3.0 №4. Контрольная работа"
    Добавил учёт случая, когда сидеть перед, чуть блише.
    
    Некрасиво написано очень, печально на сию лапшу смотреть... Да, всё Ок.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/4. Контрольная работа.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/4. Контрольная работа.py
@@ -1,6 +1,18 @@
 def main():
     '''
     '''
+    def calculate_row_and_column(position):
+        row_and_column = [-1, -1]
+        if position > 0:
+            if position % 2 == 0:
+                row_and_column[0] = position // 2
+                row_and_column[1] = 2
+            else:
+                row_and_column[0] = 1 + position // 2
+                row_and_column[1] = 1
+        return row_and_column
+
+    
     N = int(input())
     K = int(input())
     row_number_Petya = int(input())
@@ -12,25 +24,33 @@ def main():
     else:
         position_Petya = 2 * row_number_Petya
     
-    position_Vasya = -1
+    position_Vasya_before, position_Vasya_after = -1, -1
+    row_and_column_before, row_and_column_after = [-1, -1], [-1, -1]
     if position_Petya + K <= N:
-        position_Vasya = position_Petya + K
-    elif position_Petya - K >= 1:
-        position_Vasya = position_Petya - K
+        position_Vasya_before = position_Petya + K
+        row_and_column_before = calculate_row_and_column(position_Vasya_before)
+    if position_Petya - K >= 1:
+        position_Vasya_after = position_Petya - K
+        row_and_column_after = calculate_row_and_column(position_Vasya_after)
     
-    row_and_column = [-1, -1]
-    if position_Vasya > 0:
-        if position_Vasya % 2 == 0:
-            row_and_column[0] = position_Vasya // 2
-            row_and_column[1] = 2
-        else:
-            row_and_column[0] = 1 + position_Vasya // 2
-            row_and_column[1] = 1
+    row_and_column_best = [-1, -1]
+    if row_and_column_before[0] != -1 and row_and_column_after[0] != -1:
+        len_before = row_and_column_before[0] - row_number_Petya
+        len_after = row_number_Petya - row_and_column_after[0]
+        if len_before > len_after:
+            row_and_column_best = row_and_column_after
+        else: # len_1 <= len_2:
+            row_and_column_best = row_and_column_before
+    elif row_and_column_before[0] != -1:
+        row_and_column_best = row_and_column_before
+    elif row_and_column_after[0] != -1:
+        row_and_column_best = row_and_column_after
     
-    if row_and_column[0] == -1:
+    
+    if row_and_column_best[0] == -1:
         print(-1)
     else:
-        print(' '.join(map(str, row_and_column)))
+        print(' '.join(map(str, row_and_column_best)))
 
      
 if __name__ == '__main__':

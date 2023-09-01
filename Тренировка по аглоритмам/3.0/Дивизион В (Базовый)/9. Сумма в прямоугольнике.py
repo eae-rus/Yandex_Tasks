8973785b1fd952ec2d034653e3297fd044347d25
Aleksey Evdakov <eae_rus@mail.ru>
2023-09-01 21:08:20 +0300

    Изменена "3.0 №3.0 №9. Сумма в прямоугольнике"
    Исходную идею заложил верную, с префиксными суммами, но как их сделать двухмерными, не додумал. Идею решения глянул в видео. Если честно, решить её, я думаю, смог бы, если подолше подумал.
    
    Хмм... RE на третьем тесте, что-то не так с границами по ходу.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/9. Сумма в прямоугольнике.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/9. Сумма в прямоугольнике.py
@@ -2,22 +2,25 @@ def main():
     '''
     '''
     N, M, K = map(int, input().split())
-    matrix = []
+    matrix = [[0] * (N+1)]
     for _ in range(N):
         array = [0]
         for x in list(map(int, input().split())):
-            array.append(array[-1] + x)
+            array.append(x)
         matrix.append(array)
     
+    for i in range(1, N+1):
+        sum = 0
+        for k in range(1, M+1):
+            sum += matrix[i][k]
+            matrix[i][k] = matrix[i-1][k] + sum
     
     for _ in range(K):
         x1, y1, x2, y2 = map(int, input().split())
-        x1, x2 = x1-1, x2-1
-        
-        answer = 0
-        for x in range(x1, x2+1):
-            answer += matrix[x][y2] - matrix[x][y1-1]
 
+        answer = matrix[x2][y2] # 0,0 - x2, y2
+        external = matrix[x2][y1-1] + matrix[x1-1][y2] - matrix[x1-1][y1-1]
+        answer -= external
         print(answer)
         
 if __name__ == '__main__':

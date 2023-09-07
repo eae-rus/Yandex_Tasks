f6f4c255e2365c09317f8c91cdf6a56e2d89c504
Aleksey Evdakov <eae_rus@mail.ru>
2023-09-07 22:34:27 +0300

    + Добавлено "3.0 №12. Правильная скобочная последовательность"
    Попытка 1.
    Подробные тесты не устраивал, может что-то и не учёл.
    Мда... встроеный тест не учёл...

---
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/12. Правильная скобочная последовательность.py
@@ -0,0 +1,29 @@
+def main():
+    '''
+    '''
+    sequence = input()
+    stack = []
+    
+    answer = 'yes'
+    for symbol in sequence:
+        if symbol == '(' or symbol == '[' or symbol == '{':
+            stack.append(symbol)
+        else: # symbol == ')' or symbol == ']' or symbol == '}':
+            if len(stack) == 0:
+                answer = 'no'
+                break
+            if symbol == ')' and stack[-1] == '(':
+                stack.pop()
+            elif symbol == ']' and stack[-1] == '[':
+                stack.pop()
+            elif symbol == '}' and stack[-1] == '{':
+                stack.pop()
+            else:
+                answer = 'no'
+                break
+        
+    print(answer)
+    
+        
+if __name__ == '__main__':
+	main()

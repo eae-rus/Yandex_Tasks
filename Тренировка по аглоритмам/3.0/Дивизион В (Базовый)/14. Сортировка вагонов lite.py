a166a8ee989f6cf9ee2175686601df18caba203b
Aleksey Evdakov <eae_rus@mail.ru>
2023-09-09 22:59:28 +0300

    Изменена "3.0 №3.0 №14. Сортировка вагонов lite"
    Вроде поправил, хотя не уверен, что прям верно. Сейчас глянем.
    
    Нет, на том же тесте валюсь.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/14. Сортировка вагонов lite.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/14. Сортировка вагонов lite.py
@@ -23,6 +23,14 @@ def main():
             if not is_real or not stack_vagon:
                 break
             stack_vagon.append(vagon)
+
+    for _ in range(len(stack_vagon)):
+        new_vagon = stack_vagon.pop()
+        if now_vagon + 1 == new_vagon:
+            now_vagon += 1
+        else:
+            is_real = False
+            break
     
     if is_real:
         print("YES")

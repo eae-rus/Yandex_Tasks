f90174641b9131c6f28e9638258500717265321f
Aleksey Evdakov <eae_rus@mail.ru>
2023-09-09 22:56:17 +0300

    Изменена "3.0 №3.0 №14. Сортировка вагонов lite"
    Некорректно обрабатывался случай, когда пустой набор (1 вагон)
    
    Теперь на 15-ом тесте проблемы, наверное, некорректно обрабатывается выбор в конце.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/14. Сортировка вагонов lite.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/14. Сортировка вагонов lite.py
@@ -23,15 +23,6 @@ def main():
             if not is_real or not stack_vagon:
                 break
             stack_vagon.append(vagon)
-
-    while stack_vagon and vagon > stack_vagon[-1]:
-        new_vagon = stack_vagon.pop()
-        if now_vagon + 1 == new_vagon:
-            now_vagon += 1
-        else:
-            is_real = False
-            break
-    
     
     if is_real:
         print("YES")

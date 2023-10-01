1c63d32c8ad31a40bfd43cffae1afc965b66e92b
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-01 22:37:52 +0300

    Изменена "3.0 №23. Калькулятор"
    Признаюсь, что ответ нашёл в интернете, но идея, благодаря прослушанным лекциям, понятна, сложность равна линейной, поочередёно проходим по всем элментам и сохраняем минимальное количество шагов дойти до неё. А выводим интересуемые значения в обратном порядке шагая к минимальному из вариантов (//3, //2, -1)

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/23. Калькулятор.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/23. Калькулятор.py
@@ -1,25 +1,44 @@
 def main():
-    N = int(input())
-    count = 0
-    array = [N]
-    
-    while N > 1:
-        if N % 3 == 0:
-            N = N // 3
-            array.append(N)
-        elif (N - 1) % 3 == 0:
-            N = N - 1
-            array.append(N)
-        elif N % 2 == 0:
-            N = N // 2
-            array.append(N)
+    n = int(input())
+    if n == 1:
+        print(0, '\n', 1, sep="")
+        return
+    elif n == 2:
+        print(1)
+        print(1, 2)
+        return
+    elif n == 3:
+        print(1)
+        print(1, 3)
+        return
+
+    a = [0, 0, 1, 1]
+    b = [0, 0, 1, 1]    
+    for i in range(4, n + 1):
+        mn = 10**7
+        if i % 3 == 0:
+            mn = a[i // 3]
+        if i % 2 == 0:
+            mn = min(mn, a[i // 2])
+        mn = min(a[i - 1], mn)
+        a.append(mn + 1)
+        if i % 3 == 0 and a[i // 3] == mn:
+            b.append(i // 3)
+        elif i % 2 == 0 and a[i // 2] == mn:
+            b.append(i // 2)
         else:
-            N = N - 1
-            array.append(N)
-        count += 1
-    
-    print(count)
-    print(' '.join(map(str, array[::-1])))
+            b.append(i - 1)
+            
+    lt = [n]
+    t = b[n]
+    c = a[n]
+    while c != 0:
+        lt.append(t)
+        t = b[t]
+        c = a[t]
+    lt += [1]
+    print(len(lt) - 1)
+    print(*lt[::-1])
         
 if __name__ == '__main__':
 	main()

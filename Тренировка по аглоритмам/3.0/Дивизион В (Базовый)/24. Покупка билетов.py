e4785aa3f490dca995b964b4cf38ca643a7b5081
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-02 23:07:11 +0300

    + Добавлено "3.0 №24. Покупка билетов"
    Попытка 1.
    Что-то подобное уже было в разборе задач, разве что не столь оптимально по памяти. Посмотрим, заработает ли моё.
    
    Нет, на втором же тесте неверный ответ...

---
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/24. Покупка билетов.py
@@ -0,0 +1,21 @@
+def main():
+    N = int(input())
+
+    one_ticket, two_ticket, three_ticket = [0] * (N+2), [0] * (N+2), [0] * (N+2)
+    one_ticket[0], two_ticket[0], three_ticket[0] = 0, 3600*4, 3600*4
+    one_ticket[1], two_ticket[1], three_ticket[1] = 0, 3600*4, 3600*4
+    for i in range(2, N+2):
+        one_ticket[i], two_ticket[i], three_ticket[i] = map(int, input().split())
+        
+    min_time = [0, 0] # i = 0 (перед тобой на 2 шаг), 1 (перед тобой на 1 шаг)
+    for i in range(2, N+2):
+        variant_1 = one_ticket[i] + min_time[0]
+        variant_2 = two_ticket[i-1] + min_time[0]
+        variant_3 = three_ticket[i-2] + min_time[1]
+        min_time[0], min_time[1] = min_time[1], min(variant_1, variant_2, variant_3)
+        
+    print(min_time[1])
+    
+        
+if __name__ == '__main__':
+	main()

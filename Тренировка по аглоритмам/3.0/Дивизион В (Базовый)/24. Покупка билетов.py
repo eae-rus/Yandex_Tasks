fefe092eec470723bd7d5dd1b8fbe4efefc5d660
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-02 23:16:36 +0300

    Изменена "3.0 №24. Покупка билетов"
    Поправил. Были некорректно учтены шаги.
    Да, теперь всё хорошо.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/24. Покупка билетов.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/24. Покупка билетов.py
@@ -7,14 +7,14 @@ def main():
     for i in range(2, N+2):
         one_ticket[i], two_ticket[i], three_ticket[i] = map(int, input().split())
         
-    min_time = [0, 0] # i = 0 (перед тобой на 2 шаг), 1 (перед тобой на 1 шаг)
+    min_time = [0, 0, 0] # i = 0 (перед тобой на 1 шаг), 1 (перед тобой на 2 шага), 2 (перед тобой на 3 шага)
     for i in range(2, N+2):
         variant_1 = one_ticket[i] + min_time[0]
-        variant_2 = two_ticket[i-1] + min_time[0]
-        variant_3 = three_ticket[i-2] + min_time[1]
-        min_time[0], min_time[1] = min_time[1], min(variant_1, variant_2, variant_3)
+        variant_2 = two_ticket[i-1] + min_time[1]
+        variant_3 = three_ticket[i-2] + min_time[2]
+        min_time[0], min_time[1], min_time[2] = min(variant_1, variant_2, variant_3), min_time[0], min_time[1]
         
-    print(min_time[1])
+    print(min_time[0])
     
         
 if __name__ == '__main__':

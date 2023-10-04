d7760ef51a21d9c8e6807ca05cfd0a9121a4ea09
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-04 21:47:19 +0300

    Изменена "3.0 №25. Гвоздики"
    Пришлось воспользоваться теорией из разбора задач.
    
    Хмм... А теперь TL.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/25. Гвоздики.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/25. Гвоздики.py
@@ -1,33 +1,15 @@
 def main():
+    def min_len_thread(n, carnations):
+        if n == 0 or n == 1:
+            return carnations[1] - carnations[0]
+        
+        return min(min_len_thread(n-1, carnations), min_len_thread(n-2, carnations)) + (carnations[n] - carnations[n-1])
+    
     N = int(input())
     carnations = list(map(int, input().split()))
     carnations.sort()
-    # обработка левой границы
-    number_connections_the_thread = [0, 0] # достаточно помнить 2 точки: 0 - две назад, 1 - одна назад
-    smin_len_thread = carnations[1] - carnations[0]
-    number_connections_the_thread[0], number_connections_the_thread[1] = 1, 1
-    i = 2
-    # основной цикл
-    while i < N-1:
-        if carnations[i] - carnations[i-1] < carnations[i+1] - carnations[i]:
-            smin_len_thread += carnations[i] - carnations[i-1]
-            number_connections_the_thread[1] += 1
-            if number_connections_the_thread[0] == 2:
-                smin_len_thread -= carnations[i-1] - carnations[i-2]
-            
-            number_connections_the_thread[0], number_connections_the_thread[1] = number_connections_the_thread[1], 1
-            i += 1
-        else:
-            smin_len_thread += carnations[i+1] - carnations[i]
-            number_connections_the_thread[0], number_connections_the_thread[1] = 1, 1
-            i += 2
-    # обработка правой границы
-    if i == N-1:
-        smin_len_thread += carnations[i] - carnations[i-1]
-        if number_connections_the_thread[0] == 2:
-                smin_len_thread -= carnations[i-1] - carnations[i-2]
         
-    print(smin_len_thread)
+    print(min_len_thread(N-1, carnations))
     
         
 if __name__ == '__main__':

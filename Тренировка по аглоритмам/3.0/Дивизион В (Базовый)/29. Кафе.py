31d4124c1b89d24319ab74f402c90157ac176c8c
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-06 21:47:38 +0300

    Изменена "3.0 №29. Кафе"
    Попытка 1
    Доработал алгоритм. Коды можно сделать оптимальнее по вычислениям, но старался сделать понятнее сперва.
    
    Эх, ошибка в третьем... Хотя, догадываюсь где.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/29. Кафе.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/29. Кафе.py
@@ -20,6 +20,7 @@ def main():
     
     max_cost = 300*101+1
     calculate_cost = [[max_cost for _ in range(N+1)] for _ in range(N+1)]
+    direction = [["None" for _ in range(N+1)] for _ in range(N+1)]
     calculate_cost[0][0] = 0
     for day in range(N):
         for variant in range(day+1):
@@ -30,20 +31,45 @@ def main():
             
             if cost_this_day > 100:
                 new_day_cost = calculate_cost[day][variant] + cost_this_day
-                calculate_cost[day+1][variant+1] = min(calculate_cost[day+1][variant+1], new_day_cost)
+                if new_day_cost < calculate_cost[day+1][variant+1]:
+                    calculate_cost[day+1][variant+1] = new_day_cost
+                    direction[day+1][variant+1] = "variant+1"
             else:
                 new_day_cost = calculate_cost[day][variant] + cost_this_day
-                calculate_cost[day+1][variant] = min(calculate_cost[day+1][variant], new_day_cost)
+                if new_day_cost < calculate_cost[day+1][variant]:
+                    calculate_cost[day+1][variant] = new_day_cost
+                    direction[day+1][variant] = "variant"
             
             if variant > 0:
                 new_day_cost = calculate_cost[day][variant]
-                calculate_cost[day+1][variant-1] = min(calculate_cost[day+1][variant-1], new_day_cost)
-     
+                if new_day_cost < calculate_cost[day+1][variant-1]:
+                    calculate_cost[day+1][variant-1] = new_day_cost
+                    direction[day+1][variant-1] = "variant-1"
+
     min_cost = max_cost
-    for variant in range(N+1):
-        min_cost = min(min_cost, calculate_cost[N][variant])
-    
+    remaining_coupons = 0
+    for variant in range(N+1): # FIXME: можно написать в обратном порядке с прерыванием
+        if calculate_cost[N][variant] < min_cost:
+            min_cost = calculate_cost[N][variant]
+            remaining_coupons = variant
+
+    used_coupons = 0
+    days_use_coupons = []
+    remaining_coupons_i = remaining_coupons
+    for day in range(N, 0, -1):
+        if direction[day][remaining_coupons_i] == "variant+1":
+            remaining_coupons_i -= 1
+        elif direction[day][remaining_coupons_i] == "variant":
+            continue
+        else: # direction[day][remaining_coupons_i] == "variant-1":
+            used_coupons += 1
+            days_use_coupons.append(day)
+            remaining_coupons_i += 1
+
+
     print(min_cost)  
+    print(remaining_coupons, used_coupons)
+    print(" ".join(map(str, days_use_coupons)))
     
         
 if __name__ == '__main__':

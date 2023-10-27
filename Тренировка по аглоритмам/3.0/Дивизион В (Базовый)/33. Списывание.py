47ace5518f5b2eda4926c4e76137fc761d53e224
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-27 21:19:23 +0300

    Изменена "3.0 №33. Списывание"
    Почитал в интернете - оказывается я не то искал, нужно найти не циклы а проверить на разделимость по 2 цвета.
    
    Теперь всё ОК.
    
    Мда... Условие откровенно говоря не понятно...

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/33. Списывание.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/33. Списывание.py
@@ -3,19 +3,17 @@ import sys
 sys.setrecursionlimit(100000+100)
 
 def main():
-    def dfs(graph, node, previous_node, visited, path):
-        visited[node] = True
-        path.append(node)
+    def dfs(graph, node, visited, color):
+        visited[node] = color
 
+        next_color = color%2+1
         for next_node in graph[node]:
             if not visited[next_node]:
-                if dfs(graph, next_node, node, visited, path):
+                if dfs(graph, next_node, visited, next_color):
                     return True
-            elif previous_node != next_node and next_node in path:
-                # Найден цикл
+            elif visited[next_node] == color:
+                # Найден неразделимость по двойному цвету
                 return True
-
-        path.pop()
         return False
     
     N, M = list(map(int, input().split()))
@@ -27,10 +25,10 @@ def main():
             graph[node_2].append(node_1)
     
     is_cycle_found = False
-    visited = [False for _ in range(N+1)]
+    visited = [0 for _ in range(N+1)]
     for i in range(1, N+1):
         if not visited[i]:
-            if dfs(graph, i, 0, visited, []):
+            if dfs(graph, i, visited, 1):
                 is_cycle_found = True
                 break
 

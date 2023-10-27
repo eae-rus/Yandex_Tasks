d7fc28610d2696d42eadb843ad9b0ea1f7618945
Aleksey Evdakov <eae_rus@mail.ru>
2023-10-27 22:19:05 +0300

    Изменена "3.0 №35. Поиск цикла"
    Выводил весь путь, а не цикл. Добавил поиск точки начала цикла, и вывожу с конца до неё. Ответ такой же как и у них получается.
    Всё верно.

--- Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/35. Поиск цикла.py
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/35. Поиск цикла.py
@@ -9,15 +9,15 @@ def main():
 
         for next_node in graph[node]:
             if not visited[next_node]:
-                is_cycle_found, cycle_path = dfs(graph, next_node, node, visited, path)
+                is_cycle_found, cycle_path, start_cycle = dfs(graph, next_node, node, visited, path)
                 if is_cycle_found:
-                    return [True, cycle_path]
+                    return [True, cycle_path, start_cycle]
             elif previous_node != next_node and next_node in path:
                 # Найден цикл
-                return [True, path]
+                return [True, path, next_node]
 
         path.pop()
-        return [False, path]
+        return [False, path, 0]
     
     n = int(input())
     graph = [[] for _ in range(n+1)]
@@ -32,15 +32,20 @@ def main():
     visited = [False for _ in range(n+1)]
     for i in range(1, n+1):
         if not visited[i]:
-            is_cycle_found, cycle_path = dfs(graph, i, 0, visited, [])
+            is_cycle_found, cycle_path, start_cycle = dfs(graph, i, 0, visited, [])
             if is_cycle_found:
                 is_cycle_found = True
                 break
 
     if is_cycle_found:
         print("YES")
-        print(len(cycle_path))
-        print(" ".join(map(str, cycle_path)))
+        cycle_sequence = []
+        for i in range(len(cycle_path)):
+            cycle_sequence.append(cycle_path[-1-i])
+            if cycle_path[-1-i] == start_cycle:
+                break
+        print(len(cycle_sequence))
+        print(" ".join(map(str, cycle_sequence)))
     else:
         print("NO")
         

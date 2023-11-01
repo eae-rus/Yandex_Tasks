0a50891f397b7028df679b70cba3c349ea884a46
Aleksey Evdakov <eae_rus@mail.ru>
2023-11-01 23:51:45 +0300

    + Добавлено "3.0 №38. Блохи"
    Попытка 1.
    Задача вроде не очень сложная, но долгая.
    Потратил время на написание, и первичную проверку. Да и код получился приличный.
    Вроде всё норм работает на существующих тестах, сейчас проверим.
    
    Хмм... RE - будем сейчас разбираться

---
+++ Тренировка по аглоритмам/3.0/Дивизион В (Базовый)/38. Блохи.py
@@ -0,0 +1,76 @@
+def main():
+    def calculate_jump_1(node, variant):
+        if variant == 1:
+            return [node[0] - 2, node[1] + 1]
+        elif variant == 2:
+            return [node[0] - 2, node[1] - 1]
+        elif variant == 3:
+            return [node[0] - 1, node[1] - 2]
+        elif variant == 4:
+            return [node[0] + 1, node[1] - 2]
+        elif variant == 5:
+            return [node[0] + 2, node[1] - 1]
+        elif variant == 6:
+            return [node[0] + 2, node[1] + 1]
+        elif variant == 7:
+            return [node[0] + 1, node[1] + 2]
+        elif variant == 8:
+            return [node[0] - 1, node[1] + 2]
+        else:
+            return [node[0], node[1]]
+        
+    def is_valid_node(X, Y, N, M, visited_nodes):
+        if X > 0 and X <= N and Y > 0 and Y <= M:
+            if not visited_nodes[X][Y]:
+                visited_nodes[X][Y] = True
+                return True
+        return False
+    
+    N, M, feeder_X, feeder_Y, Q  = list(map(int, input().split()))
+    all_distance = 0
+    
+    fleas = []
+    for _ in range(Q):
+        fleas_X, fleas_Y = list(map(int, input().split()))
+        fleas.append([fleas_X, fleas_Y])
+    
+    for i in range(Q):
+        fleas_X, fleas_Y = fleas[i][0], fleas[i][1]
+        if fleas_X == feeder_X and fleas_Y == feeder_Y:
+            continue
+
+        visited_nodes = [[False for _ in range(N+1)] for _ in range(M+1)]
+        visited_nodes[fleas_X][fleas_Y] = True
+        
+        distance = 1
+        now_visited = []
+        now_visited.append([fleas_X, fleas_Y])
+        is_feeder_found = False
+        while len(now_visited) > 0 and not is_feeder_found:
+            next_visited = []
+            for k in range(len(now_visited)):
+                node = now_visited[k]
+                for variant in range(1, 9):
+                    jump = calculate_jump_1(node, variant)
+                    if is_valid_node(jump[0], jump[1], N, M, visited_nodes):
+                        next_visited.append(jump)
+                        
+                    if jump[0] == feeder_X and jump[1] == feeder_Y:
+                        all_distance += distance
+                        is_feeder_found = True
+                        break
+                if is_feeder_found:
+                    break
+            now_visited = next_visited
+            distance += 1
+
+        if not is_feeder_found:
+            all_distance = -1
+            break
+            
+        
+    print(all_distance)
+    
+    
+if __name__ == '__main__':
+	main()

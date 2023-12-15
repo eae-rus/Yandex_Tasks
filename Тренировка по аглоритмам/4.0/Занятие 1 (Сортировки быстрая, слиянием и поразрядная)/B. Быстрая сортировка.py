8d5d56a217026ea3bbeece29f67e44e6f96ce13b
Aleksey Evdakov <eae_rus@mail.ru>
2023-12-15 21:33:34 +0300

    Изменена "4.0 Разминка. B. Быстрая сортировка"
    Ещё одна мелкая правка, но она вряд ли что-то изменит. Хотя... Нет, теперь на 19-ом тесте TL, а ошибки работы алгоритма - нет.

--- Тренировка по аглоритмам/4.0/Занятие 1 (Сортировки быстрая, слиянием и поразрядная)/B. Быстрая сортировка.py
+++ Тренировка по аглоритмам/4.0/Занятие 1 (Сортировки быстрая, слиянием и поразрядная)/B. Быстрая сортировка.py
@@ -10,8 +10,9 @@ def main():
             new_left = left # left выходит за исследуемые границы слева
             new_mid = left + 1 # mid равен первой исследуемой точке
             new_right = right # right выходит за исследуемые границы справа
-            x = arr[new_mid]
-            arr[new_mid], arr[left + 1] = arr[left + 1], arr[new_mid]
+            i_med = (left + right) // 2
+            x = arr[i_med]
+            arr[i_med], arr[left + 1] = arr[left + 1], arr[i_med]
             while new_right - new_mid > 1:
                 i_now = new_mid + 1
                 if arr[i_now] < x:
@@ -24,7 +25,7 @@ def main():
                     new_right -= 1
                     arr[i_now], arr[new_right] = arr[new_right], arr[i_now]
                     
-            quick_sort(arr, left, new_left)
+            quick_sort(arr, left, new_left+1)
             quick_sort(arr, new_right-1, right)
             return True
     

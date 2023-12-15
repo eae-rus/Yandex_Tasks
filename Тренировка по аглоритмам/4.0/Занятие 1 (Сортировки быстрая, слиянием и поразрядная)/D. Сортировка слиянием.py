cf2378c9830dd5a291501789b9d437b245e94f0c
Aleksey Evdakov <eae_rus@mail.ru>
2023-12-15 21:51:50 +0300

    + Добавлено "4.0 Занятие 1. D. Сортировка слиянием"
    Попытка 1.
    Уже реализовывал сей алгоритм, поэтому даже не стал заморачиваться по поводу того, что ChatGPT автоматически мне предлагает решение. Да, у меня сейчас более затратно по памяти сделано, но я и с индексами раньше реализовывал, поэтому даже не стал заморачиваться.

---
+++ Тренировка по аглоритмам/4.0/Занятие 1 (Сортировки быстрая, слиянием и поразрядная)/D. Сортировка слиянием.py
@@ -0,0 +1,46 @@
+def main():
+    '''
+    '''
+    def merge_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        left = merge_sort(left)
+        right = merge_sort(right)
+        
+        return merge(left, right)
+
+    def merge(left, right):
+        merged = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+        
+        while i < len(left):
+            merged.append(left[i])
+            i += 1
+        
+        while j < len(right):
+            merged.append(right[j])
+            j += 1
+        
+        return merged
+
+    
+    N = int(input())
+    arr = list(map(int, input().split()))
+    arr = merge_sort(arr)
+    print(" ".join(map(str, arr)))
+            
+if __name__ == '__main__':
+	main()

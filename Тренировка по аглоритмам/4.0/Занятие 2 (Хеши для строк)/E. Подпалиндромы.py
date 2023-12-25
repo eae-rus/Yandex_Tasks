349aab6a6f52a392c0422521b85336389bb70fea
Aleksey Evdakov <eae_rus@mail.ru>
2023-12-25 23:23:23 +0300

    Изменена "4.0 Занятие 2. E. Подпалиндромы"
    Мой косяк, не то сохранил (описание читай в предыдущем комменте).

--- Тренировка по аглоритмам/4.0/Занятие 2 (Хеши для строк)/E. Подпалиндромы.py
+++ Тренировка по аглоритмам/4.0/Занятие 2 (Хеши для строк)/E. Подпалиндромы.py
@@ -1,41 +1,47 @@
 # E. Подпалиндромы
 # требуется изучить алгоритм Манакера
 # https://e-maxx.ru/algo/palindromes_count
+# Код приведённый ниже написан с применением ChatGPT
 
 
-def count_palindromic_substrings(s):
-    n = len(s)
-    d1 = [0] * n
-    d2 = [0] * n
+def count_palindromic_substrings(strings):
+    N = len(strings)
+    # Добавляем специальные символы "#" между символами строки и в начало и конец строки
+    modified_string = '#' + '#'.join(strings) + '#'
+    modified_length = len(modified_string)
+    
+    # Массив для хранения длин палиндромных подстрок в модифицированной строке
+    pal_lengths = [0] * modified_length
+    
+    # Позиция и длина самого правого палиндрома, который был найден
+    center = right = 0
+    
+    # Количество палиндромных подстрок
     count = 0
-
-    # Обработка нечетной длины палиндромов
-    l, r = 0, -1
-    for i in range(n):
-        k = i > r and 1 or min(d1[l + r - i], r - i + 1)
-        while i + k < n and i - k >= 0 and s[i + k] == s[i - k]:
-            k += 1
-        d1[i] = k
-        if i + k - 1 > r:
-            l, r = i - k + 1, i + k - 1
-        count += (d1[i] + 1) // 2
-
-    # Обработка четной длины палиндромов
-    l, r = 0, -1
-    for i in range(n):
-        k = i > r and 0 or min(d2[l + r - i + 1], r - i + 1)
-        while i + k < n and (i - k - 1 >= 0 if i > 0 else True) and s[i + k] == s[i - k - 1]:
-            k += 1
-        d2[i] = k
-        if i + k - 1 > r:
-            l, r = i - k, i + k - 1
-        count += (d2[i] + 1) // 2
-
+    
+    for i in range(1, modified_length - 1):
+        # Определение начальной длины палиндрома на основе уже найденных палиндромов
+        if i < right:
+            mirror = 2 * center - i
+            pal_lengths[i] = min(right - i, pal_lengths[mirror])
+        
+        # Расширение палиндрома влево и вправо
+        while i + 1 + pal_lengths[i] < modified_length and i - 1 - pal_lengths[i] >= 0 and modified_string[i + 1 + pal_lengths[i]] == modified_string[i - 1 - pal_lengths[i]]:
+            pal_lengths[i] += 1
+        
+        # Обновление самого правого палиндрома
+        if i + pal_lengths[i] > right:
+            center = i
+            right = i + pal_lengths[i]
+        
+        # Подсчет палиндромных подстрок
+        count += (pal_lengths[i] + 1) // 2
+    
     return count
 
 def main():
-    s = input()
-    answer = count_palindromic_substrings(s)
+    strings = input()
+    answer = count_palindromic_substrings(strings)
     print(answer)
 
 if __name__ == '__main__':

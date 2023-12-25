689a737e7d9ae32ff6c7e14d2205802346e64369
Aleksey Evdakov <eae_rus@mail.ru>
2023-12-25 22:36:15 +0300

    + Добавлено "4.0 Занятие 2. E. Подпалиндромы"
    Попытка 1.
    Во многом опираюсь на задачку с кубиками и призраком (предыдущую).
    
    Хмм... TL на 41-ом тесте. Странно, вроде бы делал оптимально как раз, будем смотреть.

---
+++ Тренировка по аглоритмам/4.0/Занятие 2 (Хеши для строк)/E. Подпалиндромы.py
@@ -0,0 +1,39 @@
+def main():
+    '''
+    '''
+    strings = input()
+    
+    N = len(strings)
+    h_1 = [0] * (N + 1)
+    h_2 = [0] * (N + 1)
+    x = 27+1  # прописные латинские буквы
+    degree_x = [1] * (N + 1)
+    p = 1000000007
+    
+    for i in range(1, N + 1):
+        index_char = ord(strings[i - 1]) - ord('a') + 1
+        h_1[i] = (h_1[i - 1] * x + index_char) % p
+       
+        index_char = ord(strings[-i]) - ord('a') + 1
+        h_2[i] = (h_2[i - 1] * x + index_char) % p
+
+        degree_x[i] = degree_x[i-1] * x % p
+
+    answer = N # по 1 символу
+    
+    for len_sub in range(2, N+1): # длина подпалиндрома
+        for ind in range(0, N+1-len_sub): # начало подпалиндрома
+            if len_sub <= 3: # можно убрать, но вроде бы она проще и быстрее
+                if strings[ind] == strings[ind + len_sub - 1]:
+                    answer += 1
+            else:
+                #if (h_1[ind + len_sub] - h_1[ind] * degree_x[len_sub]) % p == (h_2[N - ind] - h_2[N - ind - len_sub] * degree_x[len_sub]) % p:
+                h_1_sum = (h_1[ind + len_sub] + h_2[N - ind - len_sub] * degree_x[len_sub]) % p
+                h_2_sum = (h_2[N - ind] + h_1[ind] * degree_x[len_sub]) % p
+                if (h_1_sum == h_2_sum):
+                     answer += 1
+    
+    print(answer)
+    
+if __name__ == '__main__':
+	main()

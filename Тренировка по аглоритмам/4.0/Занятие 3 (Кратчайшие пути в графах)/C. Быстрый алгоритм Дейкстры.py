091a234e89bccc00ae4590ff33fcb8010a234d81
Aleksey Evdakov <eae_rus@mail.ru>
2023-12-27 22:45:24 +0300

    Изменена "4.0 Занятие 3. C. Быстрый алгоритм Дейкстры"
    Хммм... Я явно не оптимально и не совсем верно делал. Посмотрел лекцию, понял свой косяк, переписал.
    
    Да, теперь тесты пройдены.

--- Тренировка по аглоритмам/4.0/Занятие 3 (Кратчайшие пути в графах)/C. Быстрый алгоритм Дейкстры.py
+++ Тренировка по аглоритмам/4.0/Занятие 3 (Кратчайшие пути в графах)/C. Быстрый алгоритм Дейкстры.py
@@ -10,24 +10,28 @@ def main():
         result_dist = -1
         # Создание пустой кучи
         heap = []
-        for link in link_weights[start_node]:
-            heapq.heappush(heap, link)
+        heapq.heappush(heap, (0, start_node))
         result_dist = [ -1 for _ in range(N+1) ]
         result_dist[start_node] = 0
-        while len(heap) > 0: # можно добавить прерывание по факту что "все маршруты больше пути до конечной точки"
-            dist_link, now_node, next_node = heapq.heappop(heap)
-            if result_dist[next_node] == -1 or result_dist[now_node] + dist_link < result_dist[next_node]:
-                result_dist[next_node] = result_dist[now_node] + dist_link
-                for link in link_weights[next_node]:
-                    heapq.heappush(heap, link)
+        visited_nodes = [ False for _ in range(N+1) ]
+        while len(heap) > 0:
+            dist, now_node = heapq.heappop(heap)
+            if now_node == final_node:
+                break
+            visited_nodes[now_node] = True
+            for dist_link, next_node in link_weights[now_node]: # dist_link, next_node
+                dist_next_node = dist + dist_link
+                if visited_nodes[next_node] == False and (result_dist[next_node] == -1 or dist_next_node < result_dist[next_node]):
+                    heapq.heappush(heap, (dist_next_node, next_node))
+                    result_dist[next_node] = dist_next_node
         return result_dist[final_node]
     
     N, K = map(int, input().split())
     link_weights = [ [] for _ in range(N+1) ]
     for i in range(K):
         a, b, weights = list(map(int, input().split()))
-        link_weights[a].append((weights, a, b))
-        link_weights[b].append((weights, b, a))
+        link_weights[a].append((weights, b))
+        link_weights[b].append((weights, a))
     start_node, final_node = map(int, input().split())
                 
     dist = -1

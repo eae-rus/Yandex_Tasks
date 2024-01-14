6eeaf12462de9b7e3e52165c1dde10921fd75f3f
Aleksey Evdakov <eae_rus@mail.ru>
2024-01-15 00:46:36 +0300

    Изменена "4.0 Занятие 3. C. Максимальный разрез"
    Ох и наразвлекался, чтобы заставить РАБОТАТЬ. У меня всё зацикливания и подобные проблемы шли.
    
    Так-с, есть прогресс, теперь TL на 15-ом тесте, это радует, ибо там уже понятная суть проблем (точнее они уже другие и есть прогресс).

--- Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
+++ Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
@@ -1,62 +1,62 @@
-def is_all_visited(visited):
-    for i in visited:
-        if not i:
-            return False
-    return True
+def calc_sum_edge(adjacency_edges, max_sum, node, cut_arr_old, cut_arr_new):
+    '''
+    '''
+    for i in range(len(adjacency_edges)):
+        if cut_arr_old[i] != cut_arr_old[node]:
+            max_sum -= adjacency_edges[node][i]
+    for i in range(len(adjacency_edges)):
+        if cut_arr_new[i] != cut_arr_new[node]:
+            max_sum += adjacency_edges[node][i]
+    return max_sum
 
-def find_max_edge(node, adjacency_edges, visited):
-    new_node = 0
-    max_edge = 0
-    for i in range(1, len(adjacency_edges)):
-        if adjacency_edges[node][i] > max_edge and not visited[i]:
-            max_edge = adjacency_edges[node][i]
-            new_node = i
-    return [new_node, max_edge]
-
-def bipartite_graph(node, adjacency_edges, visited, cut_arr, cut):
-    visited[node] = True
-    cut_arr[node] = cut + 1
-    cut = (cut + 1) % 2
-
-    new_node, edge = find_max_edge(node, adjacency_edges, visited)
-    max_sum_edge = edge
-
-    if new_node != 0:
-        max_sum_edge += bipartite_graph(new_node, adjacency_edges, visited, cut_arr, cut)
-    elif cut_arr[1] != cut_arr[node]:
-        max_sum_edge += adjacency_edges[1][node]
-
-    return max_sum_edge
-
-def count_cut_pieces(adjacency_edges):
-    N = len(adjacency_edges)
+def find_max_edge(adjacency_edges, node = 1, max_sum = 0, cut_arr = []):
+    '''
+    '''
+    if cut_arr == []:
+        cut_arr = [2] * len(adjacency_edges) # начинаю с 2, т.к. просят в условии задачи
+    if node >= len(adjacency_edges):
+        return [max_sum, cut_arr]
     
-    max_sum_edge = 0
-    visited = [False] * (N+1)
-    cut_arr = [0] * (N)
-    cut = 1
-
-    for node in range(1, N):
-        if not visited[node]:
-            max_sum_edge += bipartite_graph(node, adjacency_edges, visited, cut_arr, cut)
+    # 2 
+    max_sum_answer = max_sum
+    cut_arr_answer = cut_arr.copy
     
-    max_sum = 0
-    for i in range(1, N):
-        for j in range(i+1, N):
-            if cut_arr[i] != cut_arr[j]:
-                max_sum += adjacency_edges[i][j]
+    new_max_sum, new_cut_arr = find_max_edge(adjacency_edges, node+1, max_sum, cut_arr.copy())
+    if new_max_sum > max_sum_answer:
+        max_sum_answer = new_max_sum
+        cut_arr_answer = new_cut_arr.copy()
     
-    return max_sum, cut_arr
+    # 1
+    cut_arr_copy = cut_arr.copy()
+    new_max_sum = max_sum
+    if node != 1:
+        cut_arr_copy[node] = 1 # замена (можно добавить 2,3,4 и т.д.)
+        new_max_sum = calc_sum_edge(adjacency_edges, max_sum, node, cut_arr, cut_arr_copy)
+        if new_max_sum > max_sum_answer:
+            max_sum_answer = new_max_sum
+            cut_arr_answer = cut_arr_copy.copy()
+    
+    new_max_sum, new_cut_arr = find_max_edge(adjacency_edges, node+1, new_max_sum, cut_arr_copy)
+    if new_max_sum > max_sum_answer:
+        max_sum_answer = new_max_sum
+        cut_arr_answer = new_cut_arr.copy()
+        
+    return [max_sum_answer, cut_arr_answer]
+    
 
 def main():
+    '''
+    '''
     N = int(input())
     adjacency_edges = [[0] * (N+1)]
     for _ in range(N):
         adjacency_edges.append([0] + list(map(int, input().split())))
 
-    max_sum_edge, cut_arr = count_cut_pieces(adjacency_edges)
+    # Вызываем функцию для подсчета количества разрезов
+    max_sum_edge, cut_arr = find_max_edge(adjacency_edges)
     print(max_sum_edge)
     print(" ".join(map(str, cut_arr[1:])))
+    
 
 if __name__ == '__main__':
     main()

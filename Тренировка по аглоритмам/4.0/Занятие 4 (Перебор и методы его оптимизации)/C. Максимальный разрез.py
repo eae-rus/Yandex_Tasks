ec8b03323fcae1f1980e073f058b59ecde2a6d3a
Aleksey Evdakov <eae_rus@mail.ru>
2024-01-14 22:59:27 +0300

    Изменена "4.0 Занятие 3. C. Максимальный разрез"
    Использовал код предложенный ChatGPT (хоть и не сразу)
    
    Так же на 8-ом тесте падаю.

--- Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
+++ Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
@@ -1,6 +1,4 @@
 def is_all_visited(visited):
-    '''
-    '''
     for i in visited:
         if not i:
             return False
@@ -15,50 +13,50 @@ def find_max_edge(node, adjacency_edges, visited):
             new_node = i
     return [new_node, max_edge]
 
+def bipartite_graph(node, adjacency_edges, visited, cut_arr, cut):
+    visited[node] = True
+    cut_arr[node] = cut + 1
+    cut = (cut + 1) % 2
+
+    new_node, edge = find_max_edge(node, adjacency_edges, visited)
+    max_sum_edge = edge
+
+    if new_node != 0:
+        max_sum_edge += bipartite_graph(new_node, adjacency_edges, visited, cut_arr, cut)
+    elif cut_arr[1] != cut_arr[node]:
+        max_sum_edge += adjacency_edges[1][node]
+
+    return max_sum_edge
+
 def count_cut_pieces(adjacency_edges):
-    '''
-    '''
     N = len(adjacency_edges)
     
     max_sum_edge = 0
-    node = 1
-    visited = [False] * (N)
-    visited[0] = True
+    visited = [False] * (N+1)
     cut_arr = [0] * (N)
     cut = 1
-    while not is_all_visited(visited):
-        visited[node] = True
-        cut_arr[node] = cut + 1
-        cut = (cut + 1) % 2 # Переключаем цвет, реализован простейший алгоритм
-        new_node, edge = find_max_edge(node, adjacency_edges, visited)
-        max_sum_edge += edge
-        
-        if new_node != 0:
-            node = new_node
-        elif cut_arr[1] != cut_arr[node]:
-            max_sum_edge += adjacency_edges[1][node]
-        
+
+    for node in range(1, N):
+        if not visited[node]:
+            max_sum_edge += bipartite_graph(node, adjacency_edges, visited, cut_arr, cut)
+    
     max_sum = 0
     for i in range(1, N):
         for j in range(i+1, N):
             if cut_arr[i] != cut_arr[j]:
                 max_sum += adjacency_edges[i][j]
-        
+    
     return max_sum, cut_arr
 
 def main():
-    '''
-    '''
     N = int(input())
     adjacency_edges = [[0] * (N+1)]
     for _ in range(N):
         adjacency_edges.append([0] + list(map(int, input().split())))
 
-    # Вызываем функцию для подсчета количества разрезов
     max_sum_edge, cut_arr = count_cut_pieces(adjacency_edges)
     print(max_sum_edge)
     print(" ".join(map(str, cut_arr[1:])))
-    
 
 if __name__ == '__main__':
     main()

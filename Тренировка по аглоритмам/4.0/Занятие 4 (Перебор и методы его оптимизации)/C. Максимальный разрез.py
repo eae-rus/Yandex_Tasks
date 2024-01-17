517855686fedd540809d398facf0966a7b0020a2
Aleksey Evdakov <eae_rus@mail.ru>
2024-01-17 21:34:35 +0300

    Изменена "4.0 Занятие 3. C. Максимальный разрез"
    Переработал структуру. Жаль, те же проблемы - TL.

--- Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
+++ Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/C. Максимальный разрез.py
@@ -1,45 +1,38 @@
-def calc_sum_edge(adjacency_edges, max_sum, node, cut_arr_old, cut_arr_new):
+def calc_sum_edge(adjacency_edges, max_sum, node, cut_arr):
     '''
     '''
     for i in range(len(adjacency_edges)):
-        if cut_arr_old[i] != cut_arr_old[node]:
+        if cut_arr[i] != cut_arr[node]:
             max_sum -= adjacency_edges[node][i]
+    cut_arr[node] = 1
     for i in range(len(adjacency_edges)):
-        if cut_arr_new[i] != cut_arr_new[node]:
-            max_sum += adjacency_edges[node][i]
+        if cut_arr[i] != cut_arr[node]:
+            max_sum += adjacency_edges[node][i]  
+
     return max_sum
 
-def find_max_edge(adjacency_edges, node = 1, max_sum = 0, cut_arr = []):
+def find_max_edge(adjacency_edges, node, max_sum, cut_arr):
     '''
     '''
-    if cut_arr == []:
-        cut_arr = [2] * len(adjacency_edges) # начинаю с 2, т.к. просят в условии задачи
-    if node >= len(adjacency_edges):
+    if node > len(adjacency_edges):
         return [max_sum, cut_arr]
-    
-    # 2 
+
     max_sum_answer = max_sum
-    cut_arr_answer = cut_arr.copy
-    
-    new_max_sum, new_cut_arr = find_max_edge(adjacency_edges, node+1, max_sum, cut_arr.copy())
-    if new_max_sum > max_sum_answer:
-        max_sum_answer = new_max_sum
-        cut_arr_answer = new_cut_arr.copy()
-    
-    # 1
-    cut_arr_copy = cut_arr.copy()
-    new_max_sum = max_sum
+    mow_sum = 0
+    cut_arr_answer = cut_arr.copy()
     if node != 1:
-        cut_arr_copy[node] = 1 # замена (можно добавить 2,3,4 и т.д.)
-        new_max_sum = calc_sum_edge(adjacency_edges, max_sum, node, cut_arr, cut_arr_copy)
+        mow_sum = calc_sum_edge(adjacency_edges, max_sum, node,  cut_arr)
+        if mow_sum > max_sum_answer:
+            max_sum_answer = mow_sum
+            cut_arr_answer = cut_arr.copy()
+
+    node += 1
+    while node < len(adjacency_edges):
+        new_max_sum, new_cut_arr = find_max_edge(adjacency_edges, node, mow_sum, cut_arr.copy())
         if new_max_sum > max_sum_answer:
             max_sum_answer = new_max_sum
-            cut_arr_answer = cut_arr_copy.copy()
-    
-    new_max_sum, new_cut_arr = find_max_edge(adjacency_edges, node+1, new_max_sum, cut_arr_copy)
-    if new_max_sum > max_sum_answer:
-        max_sum_answer = new_max_sum
-        cut_arr_answer = new_cut_arr.copy()
+            cut_arr_answer = new_cut_arr.copy()
+        node += 1
         
     return [max_sum_answer, cut_arr_answer]
     
@@ -53,7 +46,8 @@ def main():
         adjacency_edges.append([0] + list(map(int, input().split())))
 
     # Вызываем функцию для подсчета количества разрезов
-    max_sum_edge, cut_arr = find_max_edge(adjacency_edges)
+    cut_arr = [2] * len(adjacency_edges) # начинаю с 2, т.к. просят в условии задачи
+    max_sum_edge, cut_arr = find_max_edge(adjacency_edges, 1, 0, cut_arr)
     print(max_sum_edge)
     print(" ".join(map(str, cut_arr[1:])))
     

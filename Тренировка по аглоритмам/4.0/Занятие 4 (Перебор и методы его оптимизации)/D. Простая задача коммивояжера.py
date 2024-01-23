7c51ee17de835922e4b08e31296e9bbd2d98d610
Aleksey Evdakov <eae_rus@mail.ru>
2024-01-23 22:52:49 +0300

    + Добавлено "4.0 Занятие 4. D. Простая задача коммивояжера"
    Попытка 1.
    Вроде должно работать, но лень составлять свои схемы проверки. Вроде бы учёл многие нужные вещи, WA быть не должно. Сейчас прогоню через тесты, и в случае чего, буду смотреть конкретные случаи / ошибки.
    
    Хах, на 4-ом же тесте косяк. Мда... Не должно быть ошибок. Ладно, понятно, в целом, для этого и запустил прогнаться тесты.

---
+++ Тренировка по аглоритмам/4.0/Занятие 4 (Перебор и методы его оптимизации)/D. Простая задача коммивояжера.py
@@ -0,0 +1,42 @@
+def find_len_min_cycle(adjacency_edges, is_visited, node, sum_edge):
+    '''
+    '''
+    is_visited[node] = True
+    
+    if all(is_visited):
+        is_visited[node] = False
+        if adjacency_edges[node][1] == 0:
+            return -1
+        return sum_edge + adjacency_edges[node][1]
+    
+    answer = -1
+    for i in range(2, len(adjacency_edges)):
+        if not is_visited[i] and adjacency_edges[node][i] != 0:
+            sum_edge += adjacency_edges[node][i]
+            sum_edge_i = find_len_min_cycle(adjacency_edges, is_visited, i, sum_edge)
+            if answer == -1:
+                answer = sum_edge_i
+            elif sum_edge_i != -1:
+                answer = min(answer, sum_edge_i)
+
+    is_visited[node] = False
+    return answer
+
+def main():
+    '''
+    '''
+    N = int(input())
+    adjacency_edges = [[0] * (N+1)]
+    for _ in range(N):
+        adjacency_edges.append([0] + list(map(int, input().split())))
+    
+    is_visited = [False] * (N+1)
+    is_visited[0] = True
+    node, sum_edge = 1, 0
+    answer = 0
+    if N > 1:
+        answer = find_len_min_cycle(adjacency_edges, is_visited, node, sum_edge)
+    print(answer)
+    
+if __name__ == '__main__':
+    main()
